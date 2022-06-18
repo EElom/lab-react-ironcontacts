6da4d7bdbae088187a25ef8fe5a811e476dfd6be
Elom ESSAN <elomessan@Eloms-iMac.local>
2022-06-19 01:41:49 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }  from 'react';
+import { useState } from 'react';
+import "./App.css";
+import Contacts from "./contacts.json";
+import ContactItem from "./components/ContactItem";
+
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+
+let contactsData = [
+  Contacts[0],
+  Contacts[1],
+  Contacts[2],
+  Contacts[3],
+  Contacts[4],
+];
+
+// let contactsCopy = [...Contacts]
+// let contacts = contactsCopy.splice(0,5)
 
 function App() {
+  const [contactsPlaceholder, setContact] = useState(contactsData);
+
+  const handleAdd = () => {
+    const contactsFiltered = Contacts.filter((element => !contactsPlaceholder.includes(element)));
+    const newContact = contactsFiltered[Math.floor(Math.random() * contactsFiltered.length)];
+    const contactsCopy = [...contactsPlaceholder];
+    contactsCopy.push(newContact);
+    setContact(contactsCopy);
+  };
+
+  const handleSortByName = () => {
+    const contactsCopy = [...contactsPlaceholder];
+    console.log(contactsCopy)
+    setContact(contactsCopy.sort((contact1, contact2) =>  contact1.name.localeCompare(contact2.name)));
+  }
+
+  const handleSortByPopularity = () => {
+    const contactsCopy = [...contactsPlaceholder];
+    setContact(contactsCopy.sort((contact1, contact2) => contact1.popularity - contact2.popularity))
+  }
+
+  const displayContacts = () => {
+    return contactsPlaceholder.map((contact) => {
+      return (
+        <ContactItem
+          {...contact}
+          handleDelete={handleDelete}
+        />
+      );
+    });
+  };
+
+  const handleDelete = (name) => {
+    setContact(contactsPlaceholder.filter(contact => contact.name !== name));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button onClick={() => handleSortByName()}>Sort by name</Button>
+        <Button onClick={() => handleSortByPopularity()}>Sort by popularity</Button>
+        <Button onClick={() => handleAdd()}>Add Random Contact</Button>
+      </ButtonGroup>
+      <div className="contacts-list">
+        <div className="contact">
+          <table className='table'>
+            <tr className='table-body'>
+                <th className ='table-head'>Picture</th>
+                <th className ='table-head'>Name</th>
+                <th className ='table-head'>Popularity</th>
+                <th className ='table-head'>Oscar</th>
+                <th className ='table-head'>Emmy</th>
+                <th className ='table-head'>Delete</th>
+            </tr>
+            {displayContacts()}
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ContactItem.js
@@ -0,0 +1,22 @@
+import React, { Component }  from 'react';
+import Button from '@mui/material/Button';
+
+export default function ContactItem({ name, pictureUrl, popularity, wonOscar, wonEmmy, handleDelete}) {
+    if(false){
+      return (
+        <p>Loading...</p>
+      )
+    } else {
+      return (
+            <tr className='table-body'>
+                <td><img style={{width:100, height:150 }} src={pictureUrl} alt="no-picture" /></td>
+                <td>{name}</td>
+                <td>{popularity}</td>
+                <td>{wonOscar ? <p>🏆</p> : <p>🙈</p> }</td>
+                <td>{wonEmmy ? <p>🏆</p> : <p>🙈</p>}</td>
+                <td><Button onClick={() => handleDelete(name)}>Delete</Button></td>
+            </tr>
+      )
+    } 
+  }
+
